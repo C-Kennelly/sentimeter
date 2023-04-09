2c8fef3047e4395a6de76514fb79acb2f5737f74
Connor <knelly26@gmail.com>
2023-04-09 13:57:49 -0700

    Initial commit

---
+++ Controllers/SentimeterMainController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sentimeter.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SentimeterMainController : ControllerBase
+{
+    [HttpGet(Name = "GetSentimeterMain")]
+    public IEnumerable<SentimeterResult> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new SentimeterResult
+        {
+            normalizedSentimentScore = Random.Shared.Next(0,100),
+            normalizedTrendMagnitude = Random.Shared.Next(0,10),
+        })
+        .ToArray();
+
+    }
+}
---
+++ SentimeterResult.cs
@@ -0,0 +1,8 @@
+namespace sentimeter;
+
+public class SentimeterResult
+{
+    public int normalizedSentimentScore { get; set; }
+
+    public int normalizedTrendMagnitude {get; set; } 
+}
